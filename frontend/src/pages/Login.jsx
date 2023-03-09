1b13dff5b50bf0d764082b3c7ea12923d035e9a8
klam2k20 <klam2k17@gmail.com>
2023-03-09 16:50:07 -0500

    Redesign login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -8,41 +8,39 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   return (
-    <div className='app__login__wrapper'>
-      <div className='app__login'>
-        <div className='app__login__left'>
-          <div className='app__login__logo'>
-            <Logo />
-            <span>PULSE</span>
+    <div className='app__login'>
+      <div className='app__login__left'>
+        <div className='app__login__logo'>
+          <Logo />
+          <span>PULSE</span>
+        </div>
+        <form>
+          <h1>Hello Again!</h1>
+          <span>Welcome back! Please enter your credentials.</span>
+          <div className='app__login__form__inputs'>
+            <Input
+              type='email'
+              placeholder='Email'
+              state={email}
+              setState={setEmail}
+              className='top'
+            />
+            <Input
+              type='password'
+              placeholder='Password'
+              state={password}
+              setState={setPassword}
+              className='bottom'
+            />
           </div>
-          <form>
-            <h1>Hello Again!</h1>
-            <span>Welcome back! Please enter your credentials.</span>
-            <div className='app__login__form__inputs'>
-              <Input
-                type='email'
-                placeholder='Email'
-                state={email}
-                setState={setEmail}
-                className='top'
-              />
-              <Input
-                type='password'
-                placeholder='Password'
-                state={password}
-                setState={setPassword}
-                className='bottom'
-              />
-            </div>
-            <button>Login</button>
-          </form>
-          <span>
-            Don't have an account? <Link to='/register'>Sign Up</Link>
-          </span>
-        </div>
-        <div className='app__login__right'>
-          <img src='/heart.png' />
-        </div>
+          <button>Login</button>
+        </form>
+        <span>
+          Don't have an account? <Link to='/register'>Sign Up</Link>
+        </span>
+      </div>
+      <div className='app__login__right'>
+        <img src='/heart.png' />
       </div>
     </div>
   );
