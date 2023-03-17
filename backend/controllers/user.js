45488dae1c754045be4520ba321557720b0de294
klam2k20 <klam2k17@gmail.com>
2023-03-16 23:35:59 -0400

    Update getUserProfile and add updateUserProfile endpoints

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,35 @@
-const getUserProfile = (req, res) => {
-  return res.json(req.user);
+const User = require("../models/User");
+
+const getUserProfile = async (req, res) => {
+  if (!req.params.username) return res.json(req.user);
+
+  try {
+    const user = await User.findOne({ username: req.params.username });
+    if (!user) return res.status(404).json({ message: "User Not Found" });
+    return res.json(user);
+  } catch (err) {
+    console.log(`Get User Profile: ${err}`);
+    return res.status(500).json({ message: `Database Error: ${err}` });
+  }
 };
 
-module.exports = { getUserProfile };
+const updateUserProfile = async (req, res) => {
+  const username = req.params.username;
+  const { name, pronouns, bio, pfp } = req.body;
+
+  if (!name || !pronouns || !bio || !pfp)
+    return res.status(400).json({
+      message: "Missing one or more Required Fields: Name, Pronouns, Bio, and/or Profile Photo",
+    });
+
+  try {
+    const user = await User.findOneAndUpdate({ username }, { $set: req.body }, { new: true });
+    if (!user) return res.status(400).json({ message: "User Not Found" });
+    res.json(user);
+  } catch (err) {
+    console.log(`Update User Profile: ${err}`);
+    return res.status(500).json({ message: `Database Error: ${err}` });
+  }
+};
+
+module.exports = { getUserProfile, updateUserProfile };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,12 @@
-const { getUserProfile } = require("../controllers/user");
+const { getUserProfile, updateUserProfile } = require("../controllers/user");
 const { authenicateToken } = require("../middleware/middleware");
 
 const router = require("express").Router();
 
 router.route("/").get(authenicateToken, getUserProfile);
-router.route("/:username").get(authenicateToken, getUserProfile);
+router
+  .route("/:username")
+  .get(authenicateToken, getUserProfile)
+  .put(authenicateToken, updateUserProfile);
 
 module.exports = router;
