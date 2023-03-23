4f02baea26b416c411f412121def67c7ae50f4ff
klam2k20 <klam2k17@gmail.com>
2023-03-23 17:08:07 -0400

    Update get comment endpoints to return specific props

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -1,4 +1,5 @@
 const Comment = require("../models/Comment");
+const User = require("../models/User");
 
 const getComments = async (req, res) => {
   const postId = req.query.postId;
@@ -16,8 +17,36 @@ const getComments = async (req, res) => {
           as: "likes",
         },
       },
+      {
+        $lookup: {
+          from: "users",
+          localField: "userId",
+          foreignField: "_id",
+          as: "user",
+        },
+      },
+      {
+        $project: {
+          parentId: 1,
+          comment: 1,
+          createdAt: 1,
+          likes: {
+            $map: {
+              input: "$likes",
+              as: "like",
+              in: { _id: "$$like._id", userId: "$$like.userId", parentId: "$$like.parentId" },
+            },
+          },
+          user: {
+            $map: {
+              input: "$user",
+              as: "u",
+              in: { _id: "$$u._id", username: "$$u.username", pfp: "$$u.pfp" },
+            },
+          },
+        },
+      },
     ]);
-
     const repliesWithLikes = commentsWithLikes.filter((c) => c.parentId !== undefined);
     commentsWithLikes = commentsWithLikes.filter((c) => c.parentId === undefined);
 
--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const commentSchema = new mongoose.Schema(
   {
-    userId: { type: String, ref: "User", required: true },
+    userId: { type: mongoose.Types.ObjectId, ref: "User", required: true },
     parentId: { type: mongoose.Types.ObjectId, ref: "Comment" },
     postId: { type: String, ref: "Post", required: true },
     comment: { type: String, required: true },
