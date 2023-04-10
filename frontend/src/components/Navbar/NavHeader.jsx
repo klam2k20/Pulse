26307a805a016584f0f80a8adfca75b8a93dd795
klam2k20 <klam2k17@gmail.com>
2023-04-09 22:35:03 -0400

    Add some color varibles

--- frontend/src/components/Navbar/NavHeader.jsx
+++ frontend/src/components/Navbar/NavHeader.jsx
@@ -1,8 +1,8 @@
-import Logo from '../Logo';
 import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline';
-import '../../scss/Navbar/navheader.scss';
 import { Link } from 'react-router-dom';
 import { logoutUser } from '../../lib/apiRequests';
+import '../../scss/Navbar/navheader.scss';
+import Logo from '../Logo';
 
 function NavHeader() {
   return (
